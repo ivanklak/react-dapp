28597d397501c24b2fe80d69b626418bae3a022a
ivanklak <ivanklak17@gmail.com>
2022-01-11 18:07:57 +0300

    refactor: add wallet check; add fetch greeting (it`s free) and update greeting (free for transaction)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {ethers} from "ethers";
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+
+  const [greeting, setGreetingValue] = useState('');
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts'});
+  }
+
+  async function fetchGreeting() {
+    //if metamask (or wallet) exists in browser
+    if (typeof window.ethereum !== 'undefined') {
+      //create a provider, which takes wallet as value
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      //create a contract which pass address, ABI from compiled contract, and provider
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        //return greeting
+        console.log('data: ', data)
+      } catch (err) {
+        console.log('Error: ', err)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return null
+    if (typeof window.ethereum !== 'undefined') {
+      //user connect his wallet
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      //create signer to sign the transaction
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      setGreetingValue('')
+      //waiting for the actual transaction to be confirmed on the  blockchain
+      await transaction.wait()
+      //again, just to make sure everything is working
+      fetchGreeting()
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input
+            onChange={e => setGreetingValue(e.target.value)}
+            placeholder="Set greeting"
+            value={greeting}
+        />
       </header>
     </div>
   );
